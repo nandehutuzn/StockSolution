35c942d72154b188f43d518f71390f3c5bcffe45
ZhangNing <120902259@qq.com>
2018-03-11 15:40:14 +0800

    增加string，datatable类扩展方法 -- 2018/15/35 15:35

---
+++ StockSolution/Zn.Core.Tools/DataTableHelper.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zn.Core.Tools
+{
+    public static class DataTableHelper
+    {
+        /// <summary>
+        /// DataTable转string[] 队列(包括列名)
+        /// </summary>
+        /// <param name="dt"></param>
+        /// <returns></returns>
+        public static List<string[]> ToListArray(this DataTable dt)
+        {
+            List<string[]> resultList = new List<string[]>();
+            string[] columns = new string[dt.Columns.Count];
+            for (int i = 0; i < dt.Columns.Count; i++)
+            {
+                columns[i] = dt.Columns[i].ColumnName;
+            }
+            resultList.Add(columns);
+
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                string[] cols = new string[dt.Columns.Count];
+                for (int j = 0; j < dt.Columns.Count; j++)
+                {
+                    cols[j] = dt.Rows[i][j].ToString();
+                }
+                resultList.Add(cols);
+            }
+
+            return resultList;
+        }
+    }
+}
---
+++ StockSolution/Zn.Core.Tools/StringHelper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zn.Core.Tools
+{
+    public static class StringHelper
+    {
+        public static string Reversed(this string source)
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i = source.Length - 1; i >= 0; i--)
+            {
+                sb.Append(source[i], 1);
+            }
+
+            return sb.ToString();
+        }
+    }
+}
